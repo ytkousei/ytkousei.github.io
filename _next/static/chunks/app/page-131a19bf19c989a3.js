3defdf709aa87fe94585537adc779ce7f940b582
Kousei <160195052+ytkousei@users.noreply.github.com>
2024-03-04 11:10:13 +0900

    Add files via upload

---
+++ _next/static/chunks/app/page-131a19bf19c989a3.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[931],{6651:function(n,e,u){Promise.resolve().then(u.t.bind(u,345,23))},345:function(){}},function(n){n.O(0,[971,69,744],function(){return n(n.s=6651)}),_N_E=n.O()}]);
